621c832f257f3f094ce0be83852c11f61480e2ea
Thiago Avelino <thiagoavelinoster@gmail.com>
2011-12-31 16:06:57 -0200

    Fix future error in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,3 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
 git clone git@github.com:magnunleno/Avelino-s-vimrc.git ~/.vim
-cd ~/
-ln -s .vim/.vimrc .vimrc 
+ln -s ~/.vim/.vimrc ~/.vimrc 
