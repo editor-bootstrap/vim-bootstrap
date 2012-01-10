51925434cbc376b815855b4f3a11a24882567a61
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-01-10 19:22:00 -0200

    change bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-git clone git@github.com:magnunleno/Avelino-s-vimrc.git ~/.vim
+git clone git@github.com:avelino/.vimrc.git ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc 
