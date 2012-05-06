1041989854b3303da35d50642b68f88916ac3966
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-05-06 11:15:22 -0300

    add init git submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
 git clone git@github.com:avelino/.vimrc.git ~/.vim
+cd ~/.vim
+git submodule init && git submodule update
+cd ~/
 ln -s ~/.vim/vimrc ~/.vimrc
