300cf79744564c5b3d8e3e5db4007fee4596ad9e
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-08-16 22:44:30 -0300

    Change bootstrap alter rule create bundle

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
 git clone https://github.com/avelino/.vimrc.git ~/.vim
 cd ~/.vim
-mkdir ~/.vim/bundles/
-git submodule init && git submodule update
+git submodule init
+git submodule update
 cd ~/
 ln -s ~/.vim/vimrc ~/.vimrc
