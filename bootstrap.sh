83cf6afadf601a5b5e50a973de904f5735c57cf5
Avelino <thiago@avelino.xxx>
2016-10-30 00:53:10 -0200

    remove all python file migrate to go, ref #208

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-for i in ~/.vim ~/.vimrc ~/.gvimrc ~/.vimrc.local; do [ -e $i ] && mv $i $i.old; done
-cd ~/
-git clone https://github.com/avelino/.vimrc.git ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
-cp ~/.vim/vimrc.local.example ~/.vim/.vimrc.local
-ln -s ~/.vim/vimrc.local ~/.vimrc.local
